b3fa713fb532e9bfaf2c0bb87b3e3d9f942a1f2f
Alan Arias Herrera <aarias@cruxconsultores.local>
2020-08-06 15:47:41 -0600

    subir proyecto

---
+++ ApiPruevas1/Controllers/Personas.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ApiPruevas1.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPruevas1.Controllers
+{
+
+    [ApiController]
+    public class Personas : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("api/Personas/GetPerson")]
+        public async Task<ActionResult<int>> GetPerson(int Id)
+        {
+            try
+            {
+                Persona per = listaPersonas.GetElement(Id);
+                return Ok(per);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Personas/GetAllPerson")]
+        public async Task<ActionResult<int>> GetAllPerson()
+        {       
+            try
+            {
+                var personas = listaPersonas.GetAllElements();
+                return Ok(personas);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message.ToString());
+            }
+
+        }
+
+        [HttpPost]
+        [Route("api/Personas/AddPerson")]
+        public async Task<ActionResult> AddPerson([FromBody] Persona per)
+        {
+            try {
+                listaPersonas.addElement(per);
+                return Ok(true);
+            }
+            catch (Exception ex) {
+                return Ok(ex.Message.ToString());
+            }  
+        }
+
+        [HttpPost]
+        [Route("api/Personas/UpdatePerson")]
+        public async Task<ActionResult> UpdatePerson([FromBody] Persona per)
+        {
+            try
+            {
+                listaPersonas.UpdateElement(per);
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("api/Personas/DeletePerson")]
+        public async Task<ActionResult> DeletePerson(int id)
+        {
+            try
+            {
+                listaPersonas.RemoveElement(id);
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ ApiPruevas1/Models/Pesona.cs
@@ -0,0 +1,52 @@
+﻿using ApiPruevas1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPruevas1.Models
+{
+    public class Persona
+    {
+        public int Id { set; get; }
+        public string Nombre { set; get; }
+        public int Edad { set; get; }
+        public DateTime Fecha { set; get; }
+    }
+
+    public static class listaPersonas
+    {
+        public static List<Persona> personaList = new List<Persona>();
+
+        public static void addElement(Persona persona) {
+            persona.Id = personaList.Count + 1;
+            personaList.Add(persona);
+        }
+
+        public static Persona GetElement(int id)
+        {
+            return personaList.Where(x => x.Id == id).SingleOrDefault();
+        }
+
+        public static List<Persona> GetAllElements() {
+            return personaList;
+        }
+
+        public static void UpdateElement(Persona persona)
+        {
+            foreach (Persona per in personaList.Where(x => x.Id == persona.Id)) { 
+                per.Id = persona.Id;
+                per.Nombre = persona.Nombre;
+                per.Fecha = persona.Fecha;
+                per.Edad = persona.Edad;
+            }
+        }
+
+        public static void RemoveElement(int id)
+        {
+            personaList.RemoveAll(x => x.Id == id);
+        }
+
+
+    }
+}
